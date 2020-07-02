f0c8ca2c2e305d04b69857ebf193548ecec48890
tchoubaf <tchoubaf@hitriton.com>
2020-07-02 08:37:19 -0500

    close #1.

---
+++ src/app/news-page/news-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-page',
+  templateUrl: './news-page.page.html',
+  styleUrls: ['./news-page.page.scss'],
+})
+export class NewsPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
